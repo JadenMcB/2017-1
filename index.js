482069c7301a9ba6d529605c70a3b6aafc3d4249
JadenMcCleeryBrown <jadenmccleerybrown@seattleacademy.org>
2020-02-05 09:14:52 -0800

    Add counter to server

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log('Hello World!')
+const express = require('express')
+const app = express()
+const port = 1506
+let counter = 0
+
+app.get('/', (req, res) => res.send('Hello Jaden!'))
+
+app.get('/counter', function (req, res) {
+	counter = counter + 1;
+  res.send('counter ' + counter)
+})
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
